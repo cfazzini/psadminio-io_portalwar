596039e250cb5d3c5ed7d3552b51e6ba5358a252
cfazzini <cfazzini@gmail.com>
2018-04-28 11:59:10 -0400

    Update config_properties hash location

--- manifests/config_properties.pp
+++ manifests/config_properties.pp
@@ -7,34 +7,37 @@
 # 1. Set WebUserID, which the installer forces to PTWEBSERVER regardless of input
 # 2. Set the Web Profile after install as the installer does not accept custom values
 #
+# Updated to lookup io_portalwar::config_properties hash key as part of DPK delivered site_list hash
+# This is less correct from Puppet perspective, but keeps more in line with DPK structures, 
+# and lowers duplicated hiera data.
+#
 # @example
 #   include io_portalwar::config_properties
 class io_portalwar::config_properties (
   $pia_domain_list   = $io_portalwar::pia_domain_list,
-  $config_properties = $io_portalwar::config_properties,
 ){
 
   notify { 'Updating configuration.properties': }
 
-  $pia_domain_list.each |$domain_name, $pia_domain_info| {
+    $pia_domain_list.each |$domain_name, $pia_domain_info| {
     $ps_cfg_home_dir = $pia_domain_info['ps_cfg_home_dir']
 
-    $properties = $config_properties[$domain_name]
+
     $site_list   = $pia_domain_info['site_list']
     $site_list.each |$site_name, $site_info| {
+      $config     = "${ps_cfg_home_dir}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/psftdocs/${site_name}/configuration.properties"
+      # $title = current class name, so hiera data is keyed of class::name under site_list hash, to fit with Oracle DPK
+      $properties  = $site_info[$title]
 
-        $config   = "${ps_cfg_home_dir}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/psftdocs/${site_name}/configuration.properties"
-        $properties.each | $setting, $value | {
-
-          ini_setting { "${domain_name}, ${site_name} ${setting} ${value}" :
-            ensure  => present,
-            path    => $config,
-            section => '',
-            setting => $setting,
-            value   => $value,
-          }
+      $properties.each | $setting, $value | {
+        ini_setting { "${domain_name}, ${site_name} ${setting} ${value}" :
+          ensure  => present,
+          path    => $config,
+          section => '',
+          setting => $setting,
+          value   => $value,
         }
-
+      }
     } # end site_list
   } # end pia_domain_list
 }
